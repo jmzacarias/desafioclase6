a44b4b3ef0c981716619cb2397e76e84e48a8521
jmzacarias <juanzacarias@gmail.com>
2021-09-21 19:05:28 -0300

    segundo commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const clientes = [
     {nroCliente: 1, nombre: 'Lo de Carlitos', cuit: 'xx-xxxxxxxx-xx', domicilio: 'Curupaití 4215', pais: 'Argentina'},
     {nroCliente: 2, nombre: 'Vamo arriba', cuit: 'xx-xxxxxxxx-xx', domicilio: 'Artigas 1251', pais: 'Uruguay'},
-    {nroCliente: 3, nombre: 'Ueoneando', cuit: 'xx-xxxxxxxx-xx', domicilio: 'O Higgins 96', pais: 'Chile'},
+    {nroCliente: 3, nombre: 'Sodimac', cuit: 'xx-xxxxxxxx-xx', domicilio: 'O Higgins 96', pais: 'Chile'},
     {nroCliente: 4, nombre: 'Ferreteria el bulón', cuit: 'xx-xxxxxxxx-xx', domicilio: 'Sarmiento 132', pais: 'Argentina'}
 ];
 
@@ -14,5 +14,28 @@ let pais = prompt('Ingresá tu país');
 
 clientes.push({nroCliente: 5, nombre, cuit, domicilio, pais});
 
+clientes.sort((a, b) =>{
+    if(a.pais > b.pais) {
+        return 1;
+    }
+    if(a.pais < b.pais) {
+        return -1;
+    }
+    if(a.nroCliente > b.nroCliente) {
+        return 1;
+    }
+    if(a.nroCliente < b.nroCliente) {
+        return -1;
+    }
+    else {
+        return 0;
+    }
+});
+
 console.log(clientes);
+
+const filtrar = clientes.filter((e) => e.pais === 'Argentina');
+
+
+console.log(filtrar);
 
